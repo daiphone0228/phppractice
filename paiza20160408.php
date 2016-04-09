6295523345bf0b367535fa354667969910a00a07
DaiPhone <daiphone0228@gmail.com>
2016-04-09 23:11:23 +0900

    paiza練習追記

--- paiza20160408.php
+++ paiza20160408.php
@@ -33,7 +33,6 @@
 ?>
 
 <?php
-	// 未解決！！
     // 自分の得意な言語で
     // Let's チャレンジ！！
     $input = trim(fgets(STDIN));
@@ -44,28 +43,45 @@
         $day[] = $weather[0];
         $rain[] = $weather[1];
     }
+    //var_dump($trip[1]);
+    //var_dump($day);
+    var_dump($rain);
     $max = count($rain);
+    //var_dump($max);
     for($i=0; $i<$max; $i++){
+        $tripDuring[] = $rain[$i];
         if(count($tripDuring) == $trip[1]){
-            $tripDuring[] = $rain[$i];
-            $tripDuring = array_slice($rain, 0 , $trip[1]);
+            //$tripDuring = array_slice($rain, 0 , $trip[1]);
             $rainAverage = array_sum($tripDuring) / $trip[1];
-            $rainAverageSum[] = $rainAverage;
+            $rainAverageSum[] = floor($rainAverage);
             unset($tripDuring[0]);
-        } else {
-            $tripDuring[] = $rain[$i];
+            $tripDuring = array_merge($tripDuring);
         }
     }
-
-
-    var_dump($rain);
-    var_dump($tripDuring);
-    var_dump($rainAverage);
-
+    $rainMin = min($rainAverageSum);
+    for($i=0; $i<$max; $i++){
+        $tripDuring[] = $rain[$i];
+        if(count($tripDuring) == $trip[1]){
+            //$tripDuring = array_slice($rain, 0 , $trip[1]);
+            $rainAverage = array_sum($tripDuring) / $trip[1];
+            $rainAverageSum[] = floor($rainAverage);
+            unset($tripDuring[0]);
+            $tripDuring = array_merge($tripDuring);
+        }
+        if($trip)
+    }
+    
+    //var_dump($rain);
+    //var_dump($tripDuring);
+    //var_dump($rainAverageSum);
+    var_dump($rainMin);
+    
     // 配列を最初から順に3つずつ降水確率を足していき
     // その平均を求めて、それらが一番低いものの日程を出力する
     // array_sliceを使用し、配列のなかの値を日程分だけ取得
 ?>
+
+
 
 
 
