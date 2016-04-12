947046159edbf9759ad911f2f09192de6d4ead3e
DaiPhone <daiphone0228@gmail.com>
2016-04-13 00:35:33 +0900

    paiza練習追記

--- paiza20160411.php
+++ paiza20160411.php
@@ -40,6 +40,50 @@
         $sleepTime = date("H:i",strtotime($baseTime. "-$result minute"));
         echo $sleepTime . "\n";
     }
-    
+
+?>
+
+<?php
+    // 購入したい本の巻数を表示する
+	// 最初の行に全巻の巻数
+	// 次の行に
+    $i = trim(fgets(STDIN));
+    for($a=1; $i>=$a; $a++) {
+        $allBook[] = $a;
+    }
+    // print_r($allBook);
+    $myBookNum = trim(fgets(STDIN));
+    $myBook = trim(fgets(STDIN));
+    $myBooks = explode(" ", $myBook);
+    // print_r($myBooks);
+    $iWantBooks = array_diff($allBook, $myBooks);
+    // print_r($iWantBooks);
+    $storeBookNum = trim(fgets(STDIN));
+    $sellBook = trim(fgets(STDIN));
+    $sellBooks = explode(" ", $sellBook);
+    // print_r($sellBooks);
+    $result = array_intersect($iWantBooks, $sellBooks);
+    // var_dump($result);
+    if (empty($result)) {
+        echo "None";
+    } else if (isset($result)) {
+        echo implode(" ", $result);
+    }
+?>
+
+<?php
+    // 必要在庫数に対してすくない製品を補充する
+	// それぞれの必要発注数と価格を計算し、合計を出力する
+    $i = trim(fgets(STDIN));
+    for ($i; $i>0; $i--) {
+        $supply = trim(fgets(STDIN));
+        $price = explode(" ", $supply);
+        if ($price[0] <= $price[1]) {
+            $prices[] = 0;
+        } else {
+            $prices[] = ($price[0] - $price[1]) * $price[2];
+        }
+    }
+    echo array_sum($prices);
 ?>
 
