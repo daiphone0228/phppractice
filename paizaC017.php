171a6a29a096d2eb3718be06a1f8941e3a3bcdc4
DaiPhone <daiphone0228@gmail.com>
2016-06-02 10:33:34 -0400

    Cランクが徐々に早くなってきた

---
+++ paizaC017.php
@@ -0,0 +1,27 @@
+<?php
+    // 自分の得意な言語で
+    // Let's チャレンジ！！
+    $input = trim(fgets(STDIN));
+    $boss = explode(" ", $input); // 7 2
+    $cardNum = trim(fgets(STDIN)); // 8
+    for($i=0; $i<$cardNum; $i++){
+    	$input = trim(fgets(STDIN));
+    	$card = explode(" ", $input);
+    	// var_dump($card);
+    	if($card[0] < $boss[0]){
+    		echo "High" . "\n";
+    	} else if($card[0] > $boss[0]){
+    		echo "Low" . "\n";
+    	} else if($card[0] == $boss[0]){
+    		if($card[1] < $boss[1]){
+    			echo "Low" . "\n";
+    		} else if($card[1] > $boss[1]){
+    			echo "High" . "\n";
+    		} else{
+    			false;
+    		}
+    	} else {
+    		false;
+    	}
+    }
+?>
---
+++ paizaC021.php
@@ -0,0 +1,28 @@
+<?php
+    // 自分の得意な言語で
+    // Let's チャレンジ！！
+    $input = trim(fgets(STDIN));
+    $taihu = explode(" ", $input);
+    $minR = $taihu[2] ** 2;
+    $maxR = $taihu[3] ** 2;
+    // var_dump($minR);
+    // var_dump($maxR);
+    $xc = $taihu[0];
+    $yc = $taihu[1];
+    $pp = trim(fgets(STDIN));
+    for($i=0; $i<$pp; $i++){
+    	$input = trim(fgets(STDIN));
+    	$position = explode(" ", $input);
+    	$x = $position[0];
+    	$y = $position[1];
+    	$result = ($x-$xc) ** 2 + ($y-$yc) ** 2;
+    	// var_dump($result);
+    	if($result >= $minR && $result <= $maxR){
+    		echo "yes" . "\n";
+    	} else {
+    		echo "no" . "\n";
+    	}
+
+    }
+
+?>
